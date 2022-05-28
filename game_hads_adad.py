e4984e19034c5cb9ae92d9194e857cc8b67a557c
ahmad shalgeh <39712657+ahmadshalgeh@users.noreply.github.com>
2022-05-29 03:17:16 +0430

    Upload a number guessing game project

---
+++ game_hads_adad.py
@@ -0,0 +1,22 @@
+import random
+min = 1
+max = 99
+hads = random.randint(min,max)
+print(hads)
+javab = input('javab dorosteh??')
+while javab != 'd':
+    if javab == 'b':
+        min = hads + 1
+    elif javab == 'k':
+        max = hads - 1
+    else:
+        print('lotafan dorost rahnamaii konid k = koochaktar , b = bozorgtar , d = dorost')
+        javab = input('javab dorosteh??')
+        continue
+        
+    hads = random.randint(min,max)
+    print(hads)
+    javab = input('javab dorosteh??')
+print('----------------------------------------------------')
+print('afarin javab ', hads, ' bood barikalaaaaa')
+print('----------------------------------------------------')
